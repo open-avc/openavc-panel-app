0225625ce9324bb3a0ba91612c6025ab63a1110c
Aaron Todd <aarontodd82@gmail.com>
2026-07-10 12:21:43 -0400

    version: 0.1.0-rc7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.openavc.panel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.0-rc6"
+        versionCode = 7
+        versionName = "0.1.0-rc7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
